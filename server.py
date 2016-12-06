4ec848d8dfabe8bd6224e8f9fc10c4636ca95acd
zc8340311 <zc8340311@gmail.com>
2016-12-06 15:50:27 -0500

    working on

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.manifold import TSNE
+from sklearn.manifold import MDS
+from sklearn.manifold import Isomap
 urls = (
     '/index.html', 'index',
     '/try.html','try1',
@@ -12,8 +14,7 @@ urls = (
     "/student-mat-readable.csv","response_data",
     "/parallel.html","parallel",
     "/parallel_1.html","parallel1",
-    "/stylesheets/d3.slider.css","css",
-    "/javascripts/d3.slider.js","js"
+    "/parallel_3.html","parallel_3",
 )
 app = web.application(urls, globals())
 
@@ -26,14 +27,7 @@ class try1():
     def GET(self):
         with open(r"try.html") as f:
             return f.read()
-class css():
-    def GET(self):
-        with open(r"./stylesheets/d3.slider.css") as f:
-            return f.read()
-class js():
-    def GET(self):
-        with open(r"./javascripts/d3.slider.js") as f:
-            return f.read()
+
 class parallel():
     def GET(self):
         with open(r"parallel.html") as f:
@@ -42,6 +36,12 @@ class parallel1():
     def GET(self):
         with open(r"parallel_1.html") as f:
             return f.read()
+
+class parallel_3():
+    def GET(self):
+        with open(r"parallel_3.html") as f:
+            return f.read()
+
 class response_data():
     def GET(self):
         print "data",web.input()
@@ -62,8 +62,12 @@ class project_data():
             project_data = self.lda(X,y[:,0])
         if web.input()["method"]=="tsne":
             project_data = self.tsne(X)
+        if web.input()["method"]=="mds":
+            project_data = self.mds(X)
+        if web.input()["method"]=="isomap":
+            project_data = self.mds(X)
 
-        print web.input()["color"]
+        print "color",web.input()["color"]
         if web.input()["color"]=="trend":
             project_data = np.concatenate((project_data,y[:,1].reshape(y[:,1].size,1)),axis=1)
         elif web.input()["color"]=="give_up":
@@ -94,6 +98,15 @@ class project_data():
         tsne = TSNE()
         project_d = tsne.fit_transform(data)
         return project_d
+    def mds(self,data):
+        mds = MDS()
+        project_d = mds.fit_transform(data)
+        return project_d
+    def isomap(self,data):
+        isomap = Isomap()
+        project_d = isomap.fit_transform(data)
+        return project_d
+
     def fake_pca(self,data):
         u,c,v = np.linalg.svd(data,full_matrices=False)
         c[1] = 0
